038d082a97a829b97137f5564f4b06057d94f35a
Wambita Fana <wambitafana@gmail.com>
2019-02-22 19:56:53 +0300

    change structure of js file

--- js/script.js
+++ js/script.js
@@ -1,85 +1,15 @@
-var questions = document.getElementById('quiz')
-var results = document.getElementById('result')
-var submit = document.getElementById('submit')
-var pos = 0,
-  questions, status, choices, A, B, C, answer
-var questions = [{
-    Question1: "What method is used to change strings to numbers?",
-    choices: {
-      A: "parseInt()",
-      B: "toString()",
-      C: "toNumber()",
-    },
-    answer: "B"
-  },
-  {
-    Question2: "which built in method removes the last element from an array and returns that element?",
-    choices: {
-      A: "last()",
-      B: "get()",
-      C: "pop()",
-    },
-    answer: "C"
-  },
-  {
-    Question3: "which built in method reverses the order of the elements of an array?",
-    choices: {
-      A: "changeOrder(order)",
-      B: "reverse()",
-      c: "sort(order)",
-    },
-    answer: "B"
-  },
-  {
-    Question4: "which of the following function of an array object calls a function for each element in the array?",
-    choices: {
-      A: "concat()",
-      B: "every()",
-      C: "forEach()",
-    },
-    answer: "C"
-  },
-  {
-    Question5: "among the following keywords, which one is not  a statement?",
-    choices: {
-      A: "debugger",
-      B: "use strict",
-      C: "if",
-    },
-    answer: "B"
-  }
-];
-
-function quiz() {
-  var output = [];
-  questions.forEach(question(currentQuestion, questionNumber) => {
-    var answers = [];
-    for (letter in currentQuestion.choices) {
-      answers.push( <
-        label >
-        <
-        input type = "checkbox"
-        name = "questions${questionNumber}"
-        value = "${letter}" >
-        $ {
-          letter
-        }:
-        $ {
-          currentQuestion.choices[letter]
-        } <
-        /label>
-      );
-    }
-    output.push( <
-      div class = "question" >
-      $ {
-        currentQuestion.questions
-      } < /div> <
-      div class = "answers" > $ {
-        answers.join('')
-      } <
-      /div>
-
-    )
+$(document).ready(function check() {
+    $("form#quiz").submit(function(event) {
+      event.preventDefault();
+      var a = parseInt($("input:radio[name=choice1]:checked").val());
+      var b = parseInt($("input:radio[name=choice2]:checked").val());
+      var c = parseInt($("input:radio[name=choice3]:checked").val());
+      var d = parseInt($("input:radio[name=choice4]:checked").val());
+      var e = parseInt($("input:radio[name=choice5]:checked").val());
+  
+      var total = (a + b + c + d + e);
+      var percent = ((total / 25) * 100);
+      $("#correct").text(" your score is:" + percent + "%");
+  
+    });
   });
-};
