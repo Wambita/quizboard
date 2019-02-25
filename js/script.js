166b5966f1f31d68ae838c0024a7dbe943e7e285
Wambita Fana <wambitafana@gmail.com>
2019-02-25 10:50:15 +0300

    Add input for name , email and date

--- js/script.js
+++ js/script.js
@@ -1,32 +1,33 @@
+// U I LOGIC
 $(document).ready(function check() {
-  // var name=prompt ("Welcome to Javascript Quizboard. Please enter your name");
-   //var consent=confirm ( "Hello "+ name + "Are you ready to take the test?");
-   //if (consent === true){
-   //alert("Here we go!");
-   //} else{
-      // alert("May be next time")
-   //};
-    $("form#quiz").submit(function(event) {
-      event.preventDefault();
-      var a = parseInt($("input:radio[name=choice1]:checked").val());
-      var b = parseInt($("input:radio[name=choice2]:checked").val());
-      var c = parseInt($("input:radio[name=choice3]:checked").val());
-      var d = parseInt($("input:radio[name=choice4]:checked").val());
-      var e = parseInt($("input:radio[name=choice5]:checked").val());
-  
-      var total = (a + b + c + d + e);
-      var percent = ((total / 25) * 100);
-      $("#correct").slideToggle(" your score is:" + percent + "%");
-    
-      
-      if (percent === 100 || 80) {
-        $("#correct").fadeToggle ("EXCELLENT! YOU ARE A JAVASCRIPT GURU");
-        }else if(percent === 60){
-        $("#correct").fadeToggle("FAIR YOU CAN DO BETTER");
-        } else if (percent === 40){
-        $("#correct").fadeToggle("Below Average Work Harder next time.");
-        } else{
-        $("#correct").fadeToggle("POOR! Did you study?");
-        };
-    });
-  });
+      var name = document.getElementById("name").value;
+      var email = document.getElementById("email").value;
+      $("form#quiz").submit(function(event) {
+        event.preventDefault();
+        var a = parseInt($("input:radio[name=choice1]:checked").val());
+        var b = parseInt($("input:radio[name=choice2]:checked").val());
+        var c = parseInt($("input:radio[name=choice3]:checked").val());
+        var d = parseInt($("input:radio[name=choice4]:checked").val());
+        var e = parseInt($("input:radio[name=choice5]:checked").val());
+
+ //BUSINESS LOGIC
+var total = (a + b + c + d + e);
+var percent = ((total / 25) * 100);
+
+
+
+          if (percent > 79) {
+            $("#correct").text(name + " You scored " + percent + "%. " + "EXCELLENT! YOU ARE A JAVASCRIPT GURU");
+            $("#correct").fadeToggle();
+          } else if (percent > 59 && percent < 79) {
+            $("#correct").text(name + " You scored " + percent + "%. " + "FAIR YOU CAN DO BETTER");
+            $("#correct").fadeToggle();
+          } else if (percent > 39 && percent < 59) {
+            $("#correct").text(name + " You scored " + percent + "%. " + "Below Average Work Harder next time.");
+            $("#correct").fadeToggle();
+          } else {
+            $("#correct").text(name + " You scored " + percent + "%. " + "POOR work! Did you study?");
+            $("#correct").fadeToggle();
+          };
+        });
+      });
